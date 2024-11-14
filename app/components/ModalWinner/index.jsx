2928984e66e7f79135419c31bf826d7257ead1d1
Israel <israelhumberto99@gmail.com>
2024-11-14 02:14:50 -0300

    feat(modalwinner) raissa imagem winner

--- app/components/ModalWinner/index.jsx
+++ app/components/ModalWinner/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./modalwinner.module.css";
 import { useMachineContext } from "../../context/MachineContext";
 import { useAccountContext } from "../../context/AccountContext";
+import Image from "next/image";
 
 const ModalWinner = () => {
   const { bet } = useAccountContext();
@@ -12,6 +13,9 @@ const ModalWinner = () => {
         Parabéns! <br /> Você ganhou...
       </h2>
       <div className={styles.value}>R$ {bet * 10}</div>
+      <image className={styles.imageWin}>
+        <Image src={"/assets/raissa-feliz.jpg"} width={1000} height={1000} />
+      </image>
     </div>
   );
 };
